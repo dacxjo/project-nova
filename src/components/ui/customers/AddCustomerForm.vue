f8fb1f83c055b816b295d8346b2d2ec4dbd8e329
dacxjo <djesus1906@protonmail.com>
2021-02-20 22:17:53 +0100

    App Dialog and customer form init

---
+++ src/components/ui/customers/AddCustomerForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-1/4 h-64 bg-white rounded-lg relative z-10">
+    <form @submit.prevent="addCustomer" class="flex items-center justify-center w-full h-full">
+      <input type="submit" value="Close" />
+    </form>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup(_props, { emit }) {
+    function addCustomer() {
+      emit('customer-added');
+    }
+    return {
+      addCustomer,
+    };
+  },
+});
+</script>
--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,3 +1,41 @@
 <template>
-  <section class="w-full h-full bg-green-200 rounded-xl shadow-lg "></section>
+  <section class="w-full h-full bg-gray-200 rounded-xl shadow-lg">
+    <nav class="w-full h-20 bg-white rounded-t-lg flex items-center p-4">
+      <button @click="addCustomer" class="bg-purple-600 p-3 rounded-md text-white">
+        Add customer
+      </button>
+    </nav>
+    <app-dialog
+      id="add-customer-form-dialog"
+      @close-dialog="showCustomerForm = false"
+      :show-dialog="showCustomerForm"
+    >
+      <add-customer-form @customer-added="showCustomerForm = false"></add-customer-form>
+    </app-dialog>
+  </section>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import AppDialog from '@/components/ui/AppDialog.vue';
+import AddCustomerForm from '@/components/ui/customers/AddCustomerForm.vue';
+
+export default defineComponent({
+  components: {
+    AppDialog,
+    AddCustomerForm,
+  },
+  setup() {
+    const showCustomerForm = ref(false);
+
+    function addCustomer() {
+      showCustomerForm.value = true;
+    }
+
+    return {
+      showCustomerForm,
+      addCustomer,
+    };
+  },
+});
+</script>
