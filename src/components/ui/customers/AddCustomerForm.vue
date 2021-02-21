5e15b7c2f2e0b4a4f41b5e0f5289fc42e48b4321
dacxjo <djesus1906@protonmail.com>
2021-02-21 21:10:54 +0100

    Form created

--- src/components/ui/customers/AddCustomerForm.vue
+++ src/components/ui/customers/AddCustomerForm.vue
@@ -1,7 +1,41 @@
 <template>
   <div class="w-1/4 h-64 bg-white rounded-lg relative z-10">
-    <form @submit.prevent="addCustomer" class="flex items-center justify-center w-full h-full">
-      <input type="submit" value="Close" />
+    <form
+      @submit.prevent="addCustomer"
+      class="flex flex-col items-start justify-center w-full h-full p-4"
+    >
+      <div class="grid grid-cols-4 mb-3">
+        <label for="name" class="col-span-1 text-gray-700">Name</label>
+        <input
+          name="name"
+          id="name"
+          class="border border-gray-500 col-span-3 rounded focus:outline-none px-4"
+          type="text"
+          required
+        />
+      </div>
+      <div class="grid grid-cols-4 mb-3">
+        <label for="mail" class="col-span-1 text-gray-700">Email</label>
+        <input
+          name="mail"
+          id="mail"
+          class="border border-gray-500 col-span-3 rounded focus:outline-none px-4"
+          type="email"
+          required
+        />
+      </div>
+      <div class="grid grid-cols-4 mb-3">
+        <label for="phone" class="col-span-1 text-gray-700">Phone</label>
+        <input
+          name="phone"
+          id="phone"
+          class="border border-gray-500 col-span-3 rounded focus:outline-none px-4"
+          type="tel"
+          required
+        />
+      </div>
+
+      <input class="p-2 bg-purple-500 text-white rounded" type="submit" value="Submit" />
     </form>
   </div>
 </template>
